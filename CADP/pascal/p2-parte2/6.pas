c27ef25ca4c0db9884d13a2e2cbecba649775b30
Fede Dobal <dobalfede@gmail.com>
2024-04-21 23:18:25 -0300

    commit

---
+++ CADP/pascal/p2-parte2/6.pas
@@ -0,0 +1,29 @@
+program seis;
+    procedure leer(var precio:real ; var code:integer ; var tipo:string);
+        begin
+            readln(precio);
+            readln(code);
+            readln(tipo);
+        end;
+
+    procedure codeMasBaratos();
+        begin
+
+        end;
+
+    procedure pantMasCaro();
+        begin
+
+        end;
+
+    procedure promedio();
+        begin
+
+        end;
+var
+    precio:real;
+    code:integer;
+    tipo:string;
+begin
+    leer(precio,code,tipo);
+end.
