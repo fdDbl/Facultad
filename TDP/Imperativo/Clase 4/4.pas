f493b2a6498694a44aa752f7b6defeb91e097851
Federico Dobal <130794002+fdDbl@users.noreply.github.com>
2024-09-06 10:55:25 -0300

    hasta punto F

--- TDP/Imperativo/Clase 4/4.pas
+++ TDP/Imperativo/Clase 4/4.pas
@@ -23,10 +23,20 @@ type
 		dato : prestSinISBN;
 		sig : lista;
 	end;
+	
+	lista2 = ^nodoLista2;
+	nodoLista2 = record
+		dato : cantXISBN;
+		sig : lista2;
 
 	prestamosXISBN = record
 		ISBN : integer;
 		prest : lista;
+	end;
+
+	cantXISBN = record
+		ISBN : integer;
+		cant : integer;
 	end;
 
 	arbol1 = ^nodo1;
@@ -41,7 +51,7 @@ type
 		HI , HD : arbol2;
 	end;
 
-procedure cargarPrestamos(var a1:arbol1; var a2:arbol2; var L:lista);
+procedure cargarPrestamos(var a1:arbol1; var a2:arbol2);
 
 	procedure leerPrestamo(var p:prestamoCompleto);
 	begin
@@ -102,7 +112,6 @@ procedure cargarPrestamos(var a1:arbol1; var a2:arbol2; var L:lista);
 var
 	p1:prestamoCompleto; i:integer;
 begin
-	L:=nil;
 	i:=1;
 	writeln('<---------- LECTURA DE PRESTAMOS ---------->');
 	writeln('<----- PRESTAMO ',i,' ----->');
@@ -116,11 +125,117 @@ begin
 	end;
 end;
 
+procedure ISBNMasGrande(a:arbol1);
+
+	function calcular(a:arbol1):arbol1;
+	begin
+		if(a=nil) then calcular := nil
+		else if(a^.HD = nil) then calcular := a
+		else calcular := calcular(a^.HD);
+	end;
+
 var
-	arb1 : arbol1; arb2 : arbol2; L:lista;
+	ISBNMayor : arbol1;
+begin
+	writeln('<---------- ISBN MAS GRANDE ---------->');
+	ISBNMayor := calcular(a);
+	if(ISBNMayor = nil) then writeln('El arbol esta vacio.')
+	else writeln('Resultado: ',ISBNMayor^.dato.ISBN);
+end;
+
+procedure ISBNMasChico(a:arbol2);
+
+	function calcular(a:arbol2):arbol2;
+	begin
+		if(a=nil) then calcular := nil
+		else if(a^.HI = nil) then calcular := a
+		else calcular := calcular(a^.HI);
+	end;
+
+var
+	ISBNMenor : arbol2;
+begin
+	writeln('<---------- ISBN MAS CHICO ---------->');
+	ISBNMenor := calcular(a);
+	if(ISBNMenor = nil) then writeln('El arbol esta vacio.')
+	else writeln('Resultado: ',ISBNMenor^.dato.ISBN);
+end;
+
+procedure CantPrestamosDeSocio1(a:arbol1);
+
+	function calcular(a:arbol1; nro:integer):integer;
+	begin
+		if(a=nil) then calcular := 0
+		else if(a^.dato.nroSocio = nro) then calcular := 1 + calcular(a^.HI,nro) + calcular(a^.HD,nro)
+		else calcular := calcular(a^.HI,nro) + calcular(a^.HD,nro);
+	end;
+
+var
+	nroIngresado:integer;
+begin
+	writeln('<---------- CANTIDAD DE PRESTAMOS DE UN SOCIO: ARBOL 1 ---------->');
+	write('Ingresar numero de socio: '); readln(nroIngresado);
+	writeln('Prestamos efectuados al socio numero ',nroIngresado,': ',calcular(a,nroIngresado));
+end;
+
+procedure CantPrestamosDeSocio2(a:arbol2);
+	
+	function contarLista(L:lista; nro:integer):integer;
+	begin
+		if(L = nil) then contarLista := 0
+		else if(L^.dato.nroSocio = nro) then 
+			contarLista := 1 + contarLista(L^.sig,nro)
+		else contarLista := contarLista(L^.sig,nro);
+	end;
+
+	function contarNodos(a:arbol2; nro:integer):integer;
+	begin
+		if(a = nil) then contarNodos := 0
+		else contarNodos := 
+				contarLista(a^.dato.prest,nro) +
+				contarNodos(a^.HI,nro) +
+				contarNodos(a^.HD,nro);
+	end;
+
+var
+	nroIngresado:integer;
+begin
+	writeln('<---------- CANTIDAD DE PRESTAMOS DE UN SOCIO: ARBOL 2 ---------->');
+	write('Ingresar numero de socio: '); readln(nroIngresado);
+	writeln('Prestamos efectuados al socio numero ',nroIngresado,': ',contarNodos(a,nroIngresado));
+end;
+
+procedure PuntoF(a:arbol1);
+
+	procedure cargarNodos(a:arbol1; var L:lista2);
+	var
+		r:cantXISBN;
+	begin
+		
+	end;
+
+var
+	L:lista2;
+begin
+	L := nil;
+	cargarNodos(a,L);
+end;
+
+var
+	arb1 : arbol1; arb2 : arbol2;
 begin
 	arb1 := nil;
 	arb2 := nil;
 	
-	cargarPrestamos(arb1,arb2,L);
+	cargarPrestamos(arb1,arb2);
+	writeln;
+	ISBNMasGrande(arb1);
+	writeln;
+	ISBNMasChico(arb2);
+	writeln;
+	CantPrestamosDeSocio1(arb1);
+	writeln;
+	CantPrestamosDeSocio2(arb2);
+	writeln;
+	PuntoF(arb1);
 end.
