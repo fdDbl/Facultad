df963a3f7b36142702612d8753024d6731a9b47c
Federico Dobal <130794002+fdDbl@users.noreply.github.com>
2024-10-18 12:17:11 -0300

    Create 5b.s

---
+++ AC/Práctica 4/5b.s
@@ -0,0 +1,23 @@
+.data
+A:  .word 8
+B:  .word 6
+C:  .word 0
+
+.code
+    LD $t0, A($zero)
+    LD $t1, B($zero)
+
+    BEQ $t0, $zero, SALTO1
+        SLT $t3, $t0, $t1
+        BEQZ $t3, SALTO2
+            DADD $t2, $t1, $zero
+            SD $t2, C($zero)
+            J FINAL
+SALTO1:
+        SD $zero, C($zero)
+        J FINAL
+SALTO2:
+        DADD $t2, $t0, $t0
+        SD $t2, C($zero)
+FINAL:
+    HALT
