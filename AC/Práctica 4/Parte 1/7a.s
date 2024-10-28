60b4352beeedae28164085a488cc204c9d517c5b
Federico Dobal <130794002+fdDbl@users.noreply.github.com>
2024-10-28 13:53:16 -0300

    Update 7a.s

--- AC/Práctica 4/Parte 1/7a.s
+++ AC/Práctica 4/Parte 1/7a.s
@@ -1,3 +1,4 @@
+; 1) CALCULAR POSITIVOS
 	.data
 V: .byte 5, -3, 6, -50, 4, 3, -2, 9, 1, -6
 POS: .byte 0
@@ -18,4 +19,45 @@ sigue:
     bnez $t5, lazo
 
 sb $t3, POS($zero)  ; en este programa debería devolver 6 en POS
+halt
+
+; 2) CALCULAR MÁXIMO
+	.data
+V: .byte 5, -3, 6, -50, 4, 3, -2, 9, 1, -6
+MAX: .byte 0
+
+	.code
+dadd $t0, $zero, $zero  ; índice del vector
+dadd $t3, $zero, $zero  ; valor máximo
+daddi $t5, $zero, 10    ; cantidad de elementos del vector
+
+lazo:
+    lb $t1, V($t0)
+    slt $t2, $t1, $t3
+    bnez $t2, sigue
+        dadd $t3, $zero, $t1
+sigue:
+    daddi $t0, $t0, 1
+    daddi $t5, $t5, -1
+    bnez $t5, lazo
+
+sb $t3, MAX($zero)  ; en este programa debería devolver 9 en MAX
+halt
+
+3) MODIFICAR VALORES
+	.data
+V: .byte 5, -3, 6, -50, 4, 3, -2, 9, 1, -6
+
+	.code
+dadd $t0, $zero, $zero  ; índice del vector
+daddi $t5, $zero, 10    ; cantidad de elementos del vector
+daddi $t3, $zero, 2     ; por cuánto multiplicar
+
+lazo:
+    lb $t1, V($t0)
+    dmul $t1, $t1, $t3
+    sb $t1, V($t0)
+    daddi $t0, $t0, 1
+    daddi $t5, $t5, -1
+    bnez $t5, lazo
 halt
