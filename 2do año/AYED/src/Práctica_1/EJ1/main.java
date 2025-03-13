83a99cd865da581e022fd8ced7740e9b5c6cbbcd
Federico Dobal <dobalfede@gmail.com>
2025-03-13 16:41:07 -0300

    hecho

--- 2do año/AYED/src/Práctica_1/EJ1/main.java
+++ 2do año/AYED/src/Práctica_1/EJ1/main.java
@@ -1,6 +1,15 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+    Escriba tres métodos de clase (static) que reciban por parámetro dos números enteros (tipo
+    int) a y b e impriman todos los números enteros comprendidos entre a; b (inclusive), uno por
+    cada línea en la salida estándar. Para ello, dentro de una nueva clase escriba un método por
+    cada uno de los siguientes incisos:
+    a. Que realice lo pedido con un for.
+    b. Que realice lo pedido con un while.
+    c. Que realice lo pedido sin utilizar estructuras de control iterativas (for, while, do
+    while).
+    Por último, escriba en el método de clase main el llamado a cada uno de los métodos
+    creados, con valores de ejemplo. En su computadora, ejecute el programa y verifique
+    que se cumple con lo pedido.
  */
 package Práctica_1.EJ1;
 
--- 2do año/AYED/src/Práctica_1/EJ2/main.java
+++ 2do año/AYED/src/Práctica_1/EJ2/main.java
@@ -1,6 +1,10 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+    Escriba un método de clase que dado un número n devuelva un nuevo arreglo de tamaño n
+    con los n primeros múltiplos enteros de n mayores o iguales que 1.
+    Ejemplo: f(5) = [5; 10; 15; 20; 25]; f(k) = {n*k donde k : 1..k}
+    Agregue al programa la posibilidad de probar con distintos valores de n ingresandolos por
+    teclado, mediante el uso de System.in. La clase Scanner permite leer de forma sencilla
+    valores de entrada.
  */
 package Práctica_1.EJ2;
 
