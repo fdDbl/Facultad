5e447e0a082783c65d0bbe87aca4b21e34ce71b9
Federico Dobal <dobalfede@gmail.com>
2025-03-23 18:41:12 -0300

    hecho

--- 2do año/AYED/src/Práctica_1/EJ9/TestBalanceo.java
+++ 2do año/AYED/src/Práctica_1/EJ9/TestBalanceo.java
@@ -1,6 +1,5 @@
 package Práctica_1.EJ9;
-
-import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  *
@@ -9,32 +8,23 @@ import java.util.ArrayList;
 public class TestBalanceo {
 
     public static boolean esBalanceado(String S) {
-        // resolveré con Array
-        ArrayList<Character> caracteres = new ArrayList();
-        caracteres.add('(');    
-        caracteres.add(')');
-        caracteres.add('[');
-        caracteres.add(']');
-        caracteres.add('{');
-        caracteres.add('}');
+        Stack<Character> pila = new Stack<>();
 
-        if (!S.isEmpty()) {
-            for (int i = 0; i < S.length(); i++) {
-                Character C = S.charAt(i);
-                if (caracteres.contains(C)) {
-                    if (caracteres.indexOf(C) % 2 == 0) {
-                        int suma = 0;
-                        int j = i + 1;
-                        while (suma < S.length()) {
-                            suma++;
-                            if (!caracteres.get(caracteres.indexOf(C) + 1).equals(S.charAt(j)))
-                                j++;
-                        }
-                        if ((suma % 2 != 0) || (suma == 0))
-                            return false;
-                    }
-                } else return false;
-            } return true;
-        } else return false;
+        for (char c : S.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                pila.push(c);
+            } 
+            else if (c == ')' || c == ']' || c == '}') {
+                if (pila.isEmpty()) return false;
+                
+                char top = pila.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return pila.isEmpty();
     }
 }
