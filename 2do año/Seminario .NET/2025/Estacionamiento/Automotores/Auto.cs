6e22e9236f3f00ec0650fcdf234d12f68c3943d5
Federico Dobal <dobalfede@gmail.com>
2025-04-07 18:03:36 -0300

    hecho

---
+++ 2do año/Seminario .NET/2025/Estacionamiento/Automotores/Auto.cs
@@ -0,0 +1,19 @@
+﻿namespace Automotores;
+
+public class Auto
+{
+    private string? _marca;
+    private int _modelo;
+    
+    public Auto(String marca){
+        this._marca = marca;
+        this._modelo = DateTime.Now.Year; // se crea un 0km
+    }
+    public Auto(string marca, int modelo) {
+        this._marca = marca;
+        this._modelo = modelo;
+    }
+
+    public string ObtenerDescripción() =>
+        $"Auto. Marca: {_marca}; Modelo: {_modelo}.";
+}
---
+++ 2do año/Seminario .NET/2025/Estacionamiento/ConsolaUI/Program.cs
@@ -0,0 +1,4 @@
+﻿using Automotores;
+
+Auto a = new Auto("Renault",2020);
+Console.WriteLine(a.ObtenerDescripción());
