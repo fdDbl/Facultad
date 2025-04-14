16157f25e74c8c10041f328ef62e0d4699cc40b2
Federico Dobal <dobalfede@gmail.com>
2025-04-14 11:23:40 -0300

    .

--- 2do año/Seminario .NET/2025/Clase 4/ejercicios-teoria4/main.cs
+++ 2do año/Seminario .NET/2025/Clase 4/ejercicios-teoria4/main.cs
@@ -7,11 +7,7 @@ Punto3.Run();
 Console.SetIn(entradaOriginal);
 Punto4.Run();
 Punto5.Run();
-<<<<<<< Updated upstream:2do año/Seminario .NET/2025/Clase 4/ejercicios-teoria4/Program.cs
 Punto6.Run();
 Punto7.Run();
 
 Console.ReadKey();
-=======
-Punto6.Run();
->>>>>>> Stashed changes:2do año/Seminario .NET/2025/Clase 4/ejercicios-teoria4/main.cs
