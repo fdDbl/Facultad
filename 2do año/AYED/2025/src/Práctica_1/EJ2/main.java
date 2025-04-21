f1e5b150a1fefde7af020548eba2a5e6f6687e4b
Federico Dobal <dobalfede@gmail.com>
2025-04-21 15:32:40 -0300

    hecho

--- 2do año/AYED/2025/src/Práctica_1/EJ2/main.java
+++ 2do año/AYED/2025/src/Práctica_1/EJ2/main.java
@@ -6,7 +6,7 @@
     teclado, mediante el uso de System.in. La clase Scanner permite leer de forma sencilla
     valores de entrada.
  */
-package Práctica_1.EJ2;
+package EJ2;
 
 import java.util.Scanner;
 
