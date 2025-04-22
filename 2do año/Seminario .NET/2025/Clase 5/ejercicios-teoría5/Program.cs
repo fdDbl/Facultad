e9701716ae447f590659793ca818fdfafd3db933
Federico Dobal <dobalfede@gmail.com>
2025-04-22 15:19:45 -0300

    .

--- 2do año/Seminario .NET/2025/Clase 5/ejercicios-teoría5/Program.cs
+++ 2do año/Seminario .NET/2025/Clase 5/ejercicios-teoría5/Program.cs
@@ -1,1 +1,12 @@
 ﻿Punto1.Run();
+// Punto2.Run();
+// Punto3.Run();
+// Punto4.Run();
+// Punto5.Run();
+// Punto6.Run();
+// Punto7.Run();
+// Punto8.Run();
+// Punto9.Run();
+// Punto10.Run();
+// Punto11.Run();
+Console.ReadKey();
