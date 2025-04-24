089934d3073599f605f39a9e3e6c1f151f7c64a5
Federico Dobal <dobalfede@gmail.com>
2025-04-23 22:43:50 -0300

    hecho

--- 2do año/FOD/2025/Prácticas/Práctica 2/11/11.pas
+++ 2do año/FOD/2025/Prácticas/Práctica 2/11/11.pas
@@ -15,33 +15,33 @@ type
 
     vHorasExtras = Array[1..DIMF_CAT] of real;
 
-// procedure crearArchivoHorasExtras(var aH:archivoHoras);
-// var
-//     r: registroHoras;
-//     dep, divi, emp: integer;
-// begin
-//     rewrite(aH);
+//  procedure crearArchivoHorasExtras(var aH:archivoHoras);
+//  var
+//      r: registroHoras;
+//      dep, divi, emp: integer;
+//  begin
+//      rewrite(aH);
 //     // Crear 5 departamentos
-//     for dep := 1 to 5 do
-//     begin
-//         // Cada departamento tiene 3 divisiones
-//         for divi := 1 to 3 do
-//         begin
-//             // Cada división tiene 15 empleados
-//             for emp := 1 to 15 do
-//             begin
-//                 r.departamento := dep;
-//                 r.division := divi;
-//                 r.nroEmpleado := emp + (divi * 100) + (dep * 1000); // Asegura orden
-//                 r.categoria := 1 + random(3); // categoría entre 1 y 3
-//                 r.horasExtras := 1 + random(15); // entre 1 y 15 horas extras
-//                 write(aH, r);
-//             end;
-//         end;
-//     end;
+//      for dep := 1 to 5 do
+//      begin
+//          // Cada departamento tiene 3 divisiones
+//          for divi := 1 to 3 do
+//          begin
+//              // Cada división tiene 15 empleados
+//              for emp := 1 to 15 do
+//              begin
+//                  r.departamento := dep;
+//                  r.division := divi;
+//                  r.nroEmpleado := emp + (divi * 100) + (dep * 1000); // Asegura orden
+//                  r.categoria := 1 + random(15); // categoría entre 1 y 3
+//                  r.horasExtras := 1 + random(15); // entre 1 y 15 horas extras
+//                  write(aH, r);
+//              end;
+//          end;
+//      end;
 
-//     close(aH);
-// end;
+//      close(aH);
+//  end;
 procedure cargarHorasExtras(var v:vHorasExtras);
 var
     i:integer;
@@ -82,27 +82,31 @@ begin
         deptoAct := rH.departamento;
         totalHorasDepto := 0;
         totalMontoDepto := 0;
-        writeln('----- Departamento ', deptoAct, ' -----');
+        writeln('++ DEPARTAMENTO ', deptoAct);
         while(rH.departamento = deptoAct) do begin
             divAct := rH.division;
             totalHorasDiv := 0;
             totalMontoDiv := 0;
-            writeln('----- Division ', divAct, ' -----');
+            writeln('+ Division ', divAct);
+            writeln('/------------------------------------------------------\');
             writeln('| Numero de Empleado | Total de Hs. | Importe a cobrar |');
             writeln('| ------------------ | ------------ | ---------------- |');
             while(rH.departamento = deptoAct) and (rH.division = divAct) do begin
-                writeln('| ',rH.nroEmpleado:18, ' |', rH.horasExtras:13, ' |', (vH[rH.categoria] * rH.horasExtras):17:3, ' |');
+                writeln('| #',rH.nroEmpleado:17, ' |', rH.horasExtras:9, ' Hs. | $', (vH[rH.categoria] * rH.horasExtras):15:3, ' |');
                 totalHorasDiv := totalHorasDiv + rH.horasExtras;
                 totalMontoDiv := totalMontoDiv + vH[rH.categoria] * rH.horasExtras;
                 leer(aH,rH);
             end;
+            writeln('\------------------------------------------------------/');
             writeln('Total de horas de la division ', divAct,': ',totalHorasDiv);
             writeln('Monto total de la division ', divAct,': ',totalMontoDiv:0:3);
+            writeln('--------------------------------------------------------');
             totalHorasDepto := totalHorasDepto + totalHorasDiv;
             totalMontoDepto := totalMontoDepto + totalMontoDiv;
         end;
         writeln('Total de horas del departamento ', deptoAct,': ',totalHorasDepto);
         writeln('Monto total del departamento ', deptoAct,': ',totalMontoDepto:0:3);
+        writeln('~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-');
     end;
     close(aH);
 end.
