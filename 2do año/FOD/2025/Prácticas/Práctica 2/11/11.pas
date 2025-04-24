917b4a4e264e3c843e69bc11b26d705301c9ad62
Federico Dobal <dobalfede@gmail.com>
2025-04-23 21:56:51 -0300

    hecho

--- 2do año/FOD/2025/Prácticas/Práctica 2/11/11.pas
+++ 2do año/FOD/2025/Prácticas/Práctica 2/11/11.pas
@@ -1,4 +1,7 @@
 program generarArchivoHorasExtras;
+const
+    VALOR_ALTO = 32700;
+    DIMF_CAT = 15;
 type
     registroHoras = record
         departamento: integer;
@@ -10,52 +13,96 @@ type
 
     archivoHoras = file of registroHoras;
 
-// procedure crearArchivoHorasExtras;
+    vHorasExtras = Array[1..DIMF_CAT] of real;
+
+// procedure crearArchivoHorasExtras(var aH:archivoHoras);
 // var
-//     aH: archivoHoras;
 //     r: registroHoras;
+//     dep, divi, emp: integer;
 // begin
-//     assign(aH, 'horasExtras.dat');
 //     rewrite(aH);
-
-//     { Departamento 1, División 1 }
-//     r.departamento := 1;
-//     r.division := 1;
-
-//     r.nroEmpleado := 100; r.categoria := 3; r.horasExtras := 12; write(aH, r);
-//     r.nroEmpleado := 101; r.categoria := 2; r.horasExtras := 8; write(aH, r);
-//     r.nroEmpleado := 105; r.categoria := 1; r.horasExtras := 15; write(aH, r);
-
-//     { Departamento 1, División 2 }
-//     r.division := 2;
-
-//     r.nroEmpleado := 110; r.categoria := 2; r.horasExtras := 9; write(aH, r);
-//     r.nroEmpleado := 112; r.categoria := 3; r.horasExtras := 10; write(aH, r);
-
-//     { Departamento 2, División 1 }
-//     r.departamento := 2;
-//     r.division := 1;
-
-//     r.nroEmpleado := 200; r.categoria := 1; r.horasExtras := 6; write(aH, r);
-//     r.nroEmpleado := 201; r.categoria := 1; r.horasExtras := 7; write(aH, r);
-
-//     { Departamento 2, División 2 }
-//     r.division := 2;
-
-//     r.nroEmpleado := 210; r.categoria := 2; r.horasExtras := 4; write(aH, r);
-//     r.nroEmpleado := 211; r.categoria := 3; r.horasExtras := 11; write(aH, r);
-//     r.nroEmpleado := 215; r.categoria := 2; r.horasExtras := 5; write(aH, r);
-
-//     { Departamento 3, División 1 - solo un empleado }
-//     r.departamento := 3;
-//     r.division := 1;
-
-//     r.nroEmpleado := 300; r.categoria := 2; r.horasExtras := 13; write(aH, r);
+//     // Crear 5 departamentos
+//     for dep := 1 to 5 do
+//     begin
+//         // Cada departamento tiene 3 divisiones
+//         for divi := 1 to 3 do
+//         begin
+//             // Cada división tiene 15 empleados
+//             for emp := 1 to 15 do
+//             begin
+//                 r.departamento := dep;
+//                 r.division := divi;
+//                 r.nroEmpleado := emp + (divi * 100) + (dep * 1000); // Asegura orden
+//                 r.categoria := 1 + random(3); // categoría entre 1 y 3
+//                 r.horasExtras := 1 + random(15); // entre 1 y 15 horas extras
+//                 write(aH, r);
+//             end;
+//         end;
+//     end;
 
 //     close(aH);
 // end;
-
+procedure cargarHorasExtras(var v:vHorasExtras);
+var
+    i:integer;
+    a:text;
+    monto:real;
+    nCat:integer;
 begin
-    // crearArchivoHorasExtras;
-    
+    assign(a,'horasExtras.txt');
+    reset(a);
+    for i := 1 to DIMF_CAT do begin
+        readln(a,nCat,monto);
+        v[nCat] := monto;
+    end;
+    close(a);
+end;
+procedure leer(var a:archivoHoras; var r:registroHoras);
+begin
+    if(not eof(a)) then
+        read(a,r)
+    else
+        r.departamento := VALOR_ALTO;
+end;
+var
+    aH:archivoHoras; rH:registroHoras;
+    vH:vHorasExtras;
+    deptoAct,divAct:integer;
+    totalHorasDiv:integer; totalMontoDiv:real;
+    totalHorasDepto:integer; totalMontoDepto:real;
+begin
+    Randomize;
+    assign(aH,'horasExtras.dat');
+    // crearArchivoHorasExtras(aH);
+    cargarHorasExtras(vH);
+    // impresión del listado:
+    reset(aH);
+    leer(aH,rH);
+    while(rH.departamento < VALOR_ALTO) do begin
+        deptoAct := rH.departamento;
+        totalHorasDepto := 0;
+        totalMontoDepto := 0;
+        writeln('----- Departamento ', deptoAct, ' -----');
+        while(rH.departamento = deptoAct) do begin
+            divAct := rH.division;
+            totalHorasDiv := 0;
+            totalMontoDiv := 0;
+            writeln('----- Division ', divAct, ' -----');
+            writeln('| Numero de Empleado | Total de Hs. | Importe a cobrar |');
+            writeln('| ------------------ | ------------ | ---------------- |');
+            while(rH.departamento = deptoAct) and (rH.division = divAct) do begin
+                writeln('| ',rH.nroEmpleado:18, ' |', rH.horasExtras:13, ' |', (vH[rH.categoria] * rH.horasExtras):17:3, ' |');
+                totalHorasDiv := totalHorasDiv + rH.horasExtras;
+                totalMontoDiv := totalMontoDiv + vH[rH.categoria] * rH.horasExtras;
+                leer(aH,rH);
+            end;
+            writeln('Total de horas de la division ', divAct,': ',totalHorasDiv);
+            writeln('Monto total de la division ', divAct,': ',totalMontoDiv:0:3);
+            totalHorasDepto := totalHorasDepto + totalHorasDiv;
+            totalMontoDepto := totalMontoDepto + totalMontoDiv;
+        end;
+        writeln('Total de horas del departamento ', deptoAct,': ',totalHorasDepto);
+        writeln('Monto total del departamento ', deptoAct,': ',totalMontoDepto:0:3);
+    end;
+    close(aH);
 end.
