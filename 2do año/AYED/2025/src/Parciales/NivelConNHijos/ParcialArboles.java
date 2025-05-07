d8c49a349088738d47ab20dedd6aa9d851f140da
Fede Dobal <dobalfede@gmail.com>
2025-05-07 12:35:31 -0300

    .

---
+++ 2do año/AYED/2025/src/Parciales/NivelConNHijos/ParcialArboles.java
@@ -0,0 +1,17 @@
+package Parciales.NivelConNHijos;
+
+import Práctica_3.EJ1yEJ2ByEJ3yEJ5.GeneralTree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ParcialArboles {
+    private GeneralTree<Integer> tree;
+
+    public List<Integer> nivel(int num) {
+        List<Integer> lista = new LinkedList<>();
+        Queue<GeneralTree<Integer>> cola = new LinkedList<>();
+
+    }
+}
