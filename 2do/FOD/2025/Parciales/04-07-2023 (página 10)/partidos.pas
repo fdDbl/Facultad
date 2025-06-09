799a975b14ee86b8173d92f5e9a96d8c506cb8a3
Federico Dobal <dobalfede@gmail.com>
2025-06-09 15:01:16 -0300

    .

--- 2do/FOD/2025/Parciales/04-07-2023 (página 10)/partidos.pas
+++ 2do/FOD/2025/Parciales/04-07-2023 (página 10)/partidos.pas
@@ -49,15 +49,14 @@ begin
     leer(aP,p);
     write('Informe resumen por equipo del fútbol Argentino');
     while(p.anio < VALOR_ALTO) do begin
-        read(aP,p);
         anioAct := p.anio;
         writeln('Anio ', anioAct);
-        while((p.anio < VALOR_ALTO) and (p.anio = anioAct)) do begin
+        while(p.anio = anioAct) do begin
             torneoAct := p.codeTorneo;
             writeln('   Cod_torneo ', torneoAct);
             maxPuntos := -1;
             eqGanador := '';
-            while((p.anio < VALOR_ALTO) and (p.anio = anioAct) and (p.codeTorneo = torneoAct)) do begin
+            while((p.anio = anioAct) and (p.codeTorneo = torneoAct)) do begin
                 equipoAct := p.codeEq;
                 nomAct := p.nom;
                 writeln('       Cod_equipo ', equipoAct, ' nombre equipo ', nomAct);
@@ -68,7 +67,7 @@ begin
                 cantPPerdidos := 0;
                 cantPEmpatados := 0;
                 totalPuntos := 0;
-                while((p.anio < VALOR_ALTO) and (p.anio = anioAct) and (p.codeTorneo = torneoAct) and (p.codeEq = equipoAct)) do begin
+                while((p.anio = anioAct) and (p.codeTorneo = torneoAct) and (p.codeEq = equipoAct)) do begin
                     totalGolesFav := totalGolesFav + p.golesAFavor;
                     totalGolesContra := totalGolesContra + p.golesEnContra;
                     totalDifGoles := totalDifGoles + (p.golesAFavor - p.golesEnContra);
